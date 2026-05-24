cdc8ca1f518119143f4037f66efe63a68892f24c
iamethanclaire <ethanedwardclaire@gmail.com>
2026-05-23 18:35:43 -0700

    About Page Finished, Tools/Tech drafted

--- app/composables/about/AboutDescription.tsx
+++ app/composables/about/AboutDescription.tsx
@@ -1,5 +1,6 @@
 import EthanImage from '@/public/images/ethan_selfie.jpeg'
 import SkillDeck from './SkillDeck'
+import ToolTechGrid from './ToolTechGrid'
 
 export default function AboutTitle() {
     return (
@@ -10,19 +11,19 @@ export default function AboutTitle() {
                     About Me
                 </h2>
                 <div className="flex align-top lg:flex-row md:flex-col">
-                    <div className="px-10 flex flex-col" style={{ width: "700px"}}>
+                    <div className="px-10 flex flex-col" style={{ width: "700px" }}>
                         <p className="font-outfit text-lg text-primary mb-4">
-                            My name is Ethan Claire. I'm a Computer Science and Engineering graduate from the University of Nevada, Reno, 
-                            heading to Georgia Tech for my master's in CS with a focus in Machine Learning. During my undergraduate years 
+                            My name is Ethan Claire. I'm a Computer Science and Engineering graduate from the University of Nevada, Reno,
+                            heading to Georgia Tech for my master's in CS with a focus in Machine Learning. During my undergraduate years
                             I led research on assistive technology and human-computer interaction. Here I drafted two NSF-funded projects: ADAPT,
-                            a Chrome extension that improves web readability for users with dyslexia (published at ITNG 2026), and AssistAAC, 
-                            an iOS AAC app powered by a fine-tuned T5 transformer for grammar correction. I also designed Udephi Standard, a 
+                            a Chrome extension that improves web readability for users with dyslexia (published at ITNG 2026), and AssistAAC,
+                            an iOS AAC app powered by a fine-tuned T5 transformer for grammar correction. I also designed Udephi Standard, a
                             dyslexia-friendly typeface supporting 20+ languages.
                         </p>
                         <p className="font-outfit text-lg text-primary">
-                            Beyond my academic work, I have been an independent software developer for over seven years. In 2019, I founded 
+                            Beyond my academic work, I have been an independent software developer for over seven years. In 2019, I founded
                             Strictly Dominant Gaming, where I developed Prime Target — winner of the U.S. Congressional App Challenge.
-                            In 2023, I established Udephi with a focus on building applications that respect and protect user privacy. Looking ahead, 
+                            In 2023, I established Udephi with a focus on building applications that respect and protect user privacy. Looking ahead,
                             I am committed to creating software that makes a meaningful difference in people's lives.
                         </p>
                     </div>
@@ -37,26 +38,12 @@ export default function AboutTitle() {
                 </h2>
                 <SkillDeck />
             </div>
-            <div className="mb-10">
+            {/* <div className="mb-10">
                 <h2 className="font-chiron text-xl text-about">
-                    Tools
+                    Tools and Technologies
                 </h2>
-                <div className="px-10" style={{ width: "700px", height: "300px" }}>
-                    <p className="font-outfit text-lg text-primary">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-            </div>
-            <div className="mb-10">
-                <h2 className="font-chiron text-xl text-about">
-                    Technologies
-                </h2>
-                <div className="px-10" style={{ width: "700px", height: "300px" }}>
-                    <p className="font-outfit text-lg text-primary">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-            </div>
+                <ToolTechGrid />
+            </div> */}
         </div>
     );
 };
---
+++ app/composables/about/ToolTechGrid.tsx
@@ -0,0 +1,46 @@
+export default function ToolTechGrid() {
+    return (
+        <div className="flex flex-row justify-center flex-wrap overflow-x-auto px-12">
+            <div className="flex">
+                <img src='/images/toolsandtech/github.png' alt='Github' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/vscode.png' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/docker.png' alt='Docker' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/xcode_icon.png' alt='Xcode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/inkscape.png' alt='Inkscape' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/postgres.png' alt='PostgreSQL' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+            </div>
+            <div className="flex">
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+            </div>
+            <div className="flex">
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+                <img src='/images/toolsandtech/github_icon.jpeg' alt='VSCode' className="rounded-3xl w-full h-full object-cover" style={{ width: "125px", height: "125px" }} />
+            </div>
+        </div>
+    );
+}
+
