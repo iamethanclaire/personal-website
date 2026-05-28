0e5261a03d22de67f8e296f0f810bfb5ba9f4f3d
iamethanclaire <ethanedwardclaire@gmail.com>
2026-05-27 17:52:09 -0700

    Projects Page Finished
    Projects Page takes you to Github now. This will be iterated upon eventually.

--- app/composables/projects/ProjectCard.tsx
+++ app/composables/projects/ProjectCard.tsx
@@ -1,27 +1,31 @@
-export default function ProjectCard({ projectPhoto, projectTitle, projectDescription, projectSkills}: 
-                                    { projectPhoto: string, projectTitle: string, projectDescription: string, projectSkills: string }) {
+import { NavLink } from "react-router"
+
+export default function ProjectCard({ projectPhoto, projectTitle, projectDescription, projectSkills, projectLink }:
+    { projectPhoto: string, projectTitle: string, projectDescription: string, projectSkills: string, projectLink: string }) {
     return (
-        <div className="flex flex-col rounded-3xl shadow-md border-1 border-border" style={{ height: "600px", width: "500px" }}>
-            {/* Photo */}
-            <div className="bg-light rounded-t-3xl" style={{ height: "250px" }}>
-                <img src={projectPhoto} alt="Test" className="w-full h-full object-cover rounded-t-3xl" />
+        <NavLink to={projectLink} target="_blank">
+            <div className="flex flex-col rounded-3xl shadow-md border-1 border-border" style={{ height: "600px", width: "500px" }}>
+                {/* Photo */}
+                <div className="bg-light rounded-t-3xl" style={{ height: "250px" }}>
+                    <img src={projectPhoto} alt="Test" className="w-full h-full object-cover rounded-t-3xl" />
+                </div>
+                {/* Border */}
+                <div className="border-b-1 border-border" />
+                <div className="flex flex-1 flex-col m-4">
+                    {/* Title */}
+                    <h2 className="text-xl font-chiron mb-2 text-primary">
+                        {projectTitle}
+                    </h2>
+                    {/* Description */}
+                    <p className="text-md font-outfit mx-3 text-secondary">
+                        {projectDescription}
+                    </p>
+                    {/* Tools / Skills Used */}
+                    <p className="mt-auto text-md font-outfit m-3 text-tertiary">
+                        Skills Used: {projectSkills}
+                    </p>
+                </div>
             </div>
-            {/* Border */}
-            <div className="border-b-1 border-border" />
-            <div className="flex flex-1 flex-col m-4">
-                {/* Title */}
-                <h2 className="text-xl font-chiron mb-2 text-primary">
-                    {projectTitle}
-                </h2>
-                {/* Description */}
-                <p className="text-md font-outfit mx-3 text-secondary">
-                    {projectDescription}
-                </p>
-                {/* Tools / Skills Used */}
-                <p className="mt-auto text-md font-outfit m-3 text-tertiary">
-                    Skills Used: {projectSkills}
-                </p>
-            </div>
-        </div>
+        </NavLink>
     );
 }
--- app/composables/projects/ProjectsBody.tsx
+++ app/composables/projects/ProjectsBody.tsx
@@ -1,5 +1,5 @@
-import ProjectsTitle from "./ProjectsTitle"
 import ProjectCard from "./ProjectCard"
+
 
 export default function ProjectsBody() {
     return (
@@ -14,7 +14,27 @@ export default function ProjectsBody() {
                     projectDescription="ADAPT (A Dyslexia-Accessible Page Transformer) is a Google Chrome 
                                         extension that lets users customize their reading environment to 
                                         improve digital readability."
-                    projectSkills="JavaScript, HTML/CSS" />
+                    projectSkills="JavaScript, HTML/CSS, UI/UX Design"
+                    projectLink="https://github.com/iamethanclaire/ADAPT" 
+                />
+                <ProjectCard
+                    projectPhoto="/images/projects/assist_project.png"
+                    projectTitle="AssistAAC"
+                    projectDescription="AssistAAC is an iOS-native Augmentative and Alternative Communication 
+                                        (AAC) app that leverages Natural Language Processing to enhance the  
+                                        communication experience."
+                    projectSkills="Swift, CoreML, Accessibility Design" 
+                    projectLink="https://github.com/iamethanclaire/Assist-AAC"
+                />
+                <ProjectCard
+                    projectPhoto="/images/projects/xrb_project.png"
+                    projectTitle="XRB"
+                    projectDescription="This project is a web-based application designed for  University of Nevada, 
+                    Reno’s Physics Department that supports visualisations of X-ray binary (XRB) data and supplemental 
+                    data querying, analysis, and post-processing."
+                    projectSkills="TypeScript, HTML/CSS, Docker" 
+                    projectLink="https://github.com/iamethanclaire/xrb"
+                />
             </div>
         </div>
     );
